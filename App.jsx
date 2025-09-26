a5b54fc7e2c3f48fdf57169f49c8b38af43471b2
Siddharth Arora <siddharth.a@evpe.in>
2025-09-26 23:54:35 +0530

    added push notification

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, ActivityIndicator} from 'react-native';
+import { Alert,View, ActivityIndicator,PermissionsAndroid} from 'react-native';
 import {
   NavigationContainer,
   useNavigationContainerRef,
@@ -10,13 +10,56 @@ import EditTask from './screens/EditTask';
 import TaskList from './screens/TaskList';
 import { initDB } from './components/SqliteFunctions'; // Adjust import path accordingly
 import { Provider} from 'react-redux';
+import messaging from "@react-native-firebase/messaging"
 import store from './Redux/store';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+
   const navContainer = useNavigationContainerRef();
   const [isDBReady, setIsDBReady] = useState(false);
+
+  const requestPermission = async ()=>{
+    try {
+      const result = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+      console.log("result**", result)
+      console.log("result**2", PermissionsAndroid.RESULTS.GRANTED)
+      if(result === PermissionsAndroid.RESULTS.GRANTED){
+        console.log('requesting for token');
+        requestToken()
+      }else{
+        console.log("Permission Denied")
+      }
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
+  const requestToken = async ()=>{
+    try {
+      await messaging().registerDeviceForRemoteMessages();
+      const token = await messaging().getToken();
+      console.log("token**", token)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    requestPermission()
+  }, [])
+
+   /** foreground notification */
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('A new push notification arrived', JSON.stringify(remoteMessage));
+      Alert.alert('A new push notification arrived', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
 
   useEffect(() => {
     const initializeDatabase = async () => {
@@ -29,7 +72,7 @@ const App = () => {
     };
     initializeDatabase();
   }, []);
-
+   
   if (!isDBReady) {
     return (
       <View className="flex-1 justify-center items-center bg-white">
@@ -37,6 +80,8 @@ const App = () => {
       </View>
     );
   }
+
+  
 
   return (
     <Provider store={store}>
